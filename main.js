84d3d31ff0025b197e1f82d6765fe6e6b030c9ec
Rohan030101 <janraorohan2@gmail.com>
2022-07-27 20:44:38 -0700

    First Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+setInterval(showTime, 1000);
+function showTime() {
+  let time = new Date();
+  let hours = time.getHours();
+  let mins = time.getMinutes();
+  let sec = time.getSeconds();
+  let am_pm = "PM";
+
+  if (hours > 12) {
+    hours -= 12;
+    am_pm = "PM";
+  }
+  if (hours == 0) {
+    hours = 12;
+    am_pm = "AM";
+  }
+
+  hours = hours < 10 ? "0" + hours : hours;
+  mins = mins < 10 ? "0" + mins : mins;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  let currentTime = hours + ":" + mins + ":" + sec + ":" + am_pm;
+  document.getElementById("time").innerHTML = currentTime;
+}
+showTime();
